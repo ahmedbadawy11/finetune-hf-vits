d6c53f914184fb7d9c4c290c0954a527720ea1bf
Ahmed Ibraheem Ahmed Ali <59053820+ahmedbadawy11@users.noreply.github.com>
2024-12-22 15:13:20 +0200

    update save

--- run_vits_finetuning.py
+++ run_vits_finetuning.py
@@ -1252,15 +1252,14 @@ def main():
                         # Push to Hub every 10 epochs
                         if epoch % 10 == 0:
                             logger.info(f"Pushing model to Hub at epoch {epoch}...")
-                            # model = accelerator.unwrap_model(model)
-                            # model.save_pretrained(training_args.output_dir)
+                            model = accelerator.unwrap_model(model)
+                            model.save_pretrained(training_args.output_dir)
 
                             if training_args.push_to_hub:
-                                VitsModel.from_pretrained(save_path).push_to_hub(training_args.hub_model_id)
+                                VitsModel.from_pretrained(training_args.output_dir).push_to_hub(training_args.hub_model_id)
                                 feature_extractor.push_to_hub(training_args.hub_model_id)
                                 tokenizer.push_to_hub(training_args.hub_model_id)
                                 logger.info("Model pushed to Hub.")
-
 
 
             logs = {
