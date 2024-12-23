b2932244f689065547a72c7792828068a7109922
Ahmed Ibraheem Ahmed Ali <59053820+ahmedbadawy11@users.noreply.github.com>
2024-12-23 11:56:20 +0200

    train batches

--- run_vits_finetuning.py
+++ run_vits_finetuning.py
@@ -1247,19 +1247,19 @@ def main():
                         save_path = os.path.join(training_args.output_dir, f"checkpoint-{global_step}")
                         accelerator.save_state(save_path)
                         logger.info(f"Saved state to {save_path}")
-                         # i added this 
+                        #  # i added this 
             
-                        # Push to Hub every 10 epochs
-                        if epoch % 5 == 0:
-                            logger.info(f"Pushing model to Hub at epoch {epoch}...")
-                            model = accelerator.unwrap_model(model)
-                            model.save_pretrained(training_args.output_dir)
+                        # # Push to Hub every 10 epochs
+                        # if epoch % 5 == 0:
+                        #     logger.info(f"Pushing model to Hub at epoch {epoch}...")
+                        #     model = accelerator.unwrap_model(model)
+                        #     model.save_pretrained(training_args.output_dir)
 
-                            if training_args.push_to_hub:
-                                VitsModel.from_pretrained(training_args.output_dir).push_to_hub(training_args.hub_model_id)
-                                feature_extractor.push_to_hub(training_args.hub_model_id)
-                                tokenizer.push_to_hub(training_args.hub_model_id)
-                                logger.info("Model pushed to Hub.")
+                        #     if training_args.push_to_hub:
+                        #         VitsModel.from_pretrained(training_args.output_dir).push_to_hub(training_args.hub_model_id)
+                        #         feature_extractor.push_to_hub(training_args.hub_model_id)
+                        #         tokenizer.push_to_hub(training_args.hub_model_id)
+                        #         logger.info("Model pushed to Hub.")
 
 
             logs = {
